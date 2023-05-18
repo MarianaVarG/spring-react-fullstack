860c9d5b3069227b54af21deb3681a72b7080e52
MarianaVG <marianavarelagomez18@gmail.com>
2023-05-18 10:31:15 +0200

    Router configuration

--- react-fronend/src/App.js
+++ react-fronend/src/App.js
@@ -1,12 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import Navigation from "./layouts/Navigation";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Posts"
 
 
 function App() {
   return (
-    <div>
-      <Navigation></Navigation>
-    </div>
+    <Router>
+      <div> <Navigation /> </div>
+      <Container>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/signin" element={<SignIn />} />
+        </Routes>
+      </Container>
+    </Router>
+
   );
 }
 
